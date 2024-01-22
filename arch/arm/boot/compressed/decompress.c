6e85ccefd85e6828e7d4d27428dc9d0baf2f2f3b
Randy DuCharme <radio.ad5gb@gmail.com>
2024-01-22 12:11:54 -0600

    Enable kernel zstd compression.

--- arch/arm/boot/compressed/decompress.c
+++ arch/arm/boot/compressed/decompress.c
@@ -59,7 +59,11 @@ extern char * strchrnul(const char *, int);
 #include "../../../../lib/decompress_unlz4.c"
 #endif
 
-int do_decompress(u8 *input, int len, u8 *output, void (*error)(char *x))
+#ifdef CONFIG_KERNEL_ZSTD
+#include "../../../../lib/decompress_unzstd.c"
+#endif
+
+int do_decompress(u8 *input, int len, u8 *output, int outlen, void (*error)(char *x))
 {
-	return __decompress(input, len, NULL, NULL, output, 0, NULL, error);
+	return __decompress(input, len, NULL, NULL, output, outlen, NULL, error);
 }
